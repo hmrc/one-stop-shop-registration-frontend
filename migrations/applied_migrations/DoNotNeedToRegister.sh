0388cc043bb244b5c7798ffa92cda106c52cafa4
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2021-06-13 08:57:36 +0100

    Initial scaffold of Do Not Need to Register

---
+++ migrations/applied_migrations/DoNotNeedToRegister.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration DoNotNeedToRegister"
+
+echo "Adding routes to conf/app.routes"
+echo "" >> ../conf/app.routes
+echo "GET        /doNotNeedToRegister                       controllers.DoNotNeedToRegisterController.onPageLoad()" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "doNotNeedToRegister.title = doNotNeedToRegister" >> ../conf/messages.en
+echo "doNotNeedToRegister.heading = doNotNeedToRegister" >> ../conf/messages.en
+
+echo "Migration DoNotNeedToRegister completed"
