5679afb549142a159c1b50f86a6f5b7b86d773de
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2021-06-17 09:01:00 +0100

    Add an allowlist filter based on VRN

---
+++ test/controllers/actions/VrnAllowListFilterSpec.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import base.SpecBase
+import models.requests.IdentifierRequest
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.mvc.Result
+import play.api.mvc.Results.Redirect
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.FeatureFlagService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class VrnAllowListFilterSpec extends SpecBase with MockitoSugar with BeforeAndAfterEach {
+
+  class Harness(features: FeatureFlagService) extends VrnAllowListFilterImpl(features) {
+    def callFilter(request: IdentifierRequest[_]): Future[Option[Result]] = filter(request)
+  }
+
+  ".filter" - {
+
+    "when restricting access using the VRN allow list is enabled" - {
+
+      "and the user's VRN is in the allow list" - {
+
+        "must return None" in {
+
+          val app =
+            applicationBuilder(None)
+              .configure(
+                "restrict-access-using-vrn-allow-list" -> true,
+                "vrn-allow-list"                       -> Seq(vrn.value),
+                "vrn-blocked-redirect-url"             -> "foo"
+              )
+              .build()
+
+          running(app) {
+            val request    = IdentifierRequest(FakeRequest(), testCredentials, vrn)
+            val features   = app.injector.instanceOf[FeatureFlagService]
+            val controller = new Harness(features)
+
+            val result = controller.callFilter(request).futureValue
+
+            result must not be defined
+          }
+        }
+      }
+
+      "and the user's VRN is not in the allow list" - {
+
+        "must Redirect to the redirect URL" in {
+
+          val app =
+            applicationBuilder(None)
+              .configure(
+                "features.restrict-access-using-vrn-allow-list" -> true,
+                "features.vrn-allow-list"                       -> Seq.empty,
+                "features.vrn-blocked-redirect-url"             -> "foo"
+              )
+              .build()
+
+          running(app) {
+            val request    = IdentifierRequest(FakeRequest(), testCredentials, vrn)
+            val features   = app.injector.instanceOf[FeatureFlagService]
+            val controller = new Harness(features)
+
+            val result = controller.callFilter(request).futureValue
+
+            result.value mustEqual Redirect("foo")
+          }
+        }
+      }
+    }
+
+    "when restricting access using the VRN allow list is disabled" - {
+
+      "and the user's VRN is not in the allow list" - {
+
+        "must return None" in {
+
+          val app =
+            applicationBuilder(None)
+              .configure(
+                "restrict-access-using-vrn-allow-list" -> false,
+                "vrn-allow-list" -> Seq.empty,
+                "vrn-blocked-redirect-url" -> "foo"
+              )
+              .build()
+
+          running(app) {
+            val request = IdentifierRequest(FakeRequest(), testCredentials, vrn)
+            val features = app.injector.instanceOf[FeatureFlagService]
+            val controller = new Harness(features)
+
+            val result = controller.callFilter(request).futureValue
+
+            result must not be defined
+          }
+        }
+      }
+    }
+  }
+}
