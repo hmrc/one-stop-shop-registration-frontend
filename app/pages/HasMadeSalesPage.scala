9f8bd949d9df3ace90d0ba2c868092e6f20ddad9
darbyo <darbyo@users.noreply.github.com>
2021-08-12 14:35:16 +0100

    Add clean up behaviour for HasMadeSales

--- app/pages/HasMadeSalesPage.scala
+++ app/pages/HasMadeSalesPage.scala
@@ -21,6 +21,8 @@ import models.{CheckMode, NormalMode, UserAnswers}
 import play.api.libs.json.JsPath
 import play.api.mvc.Call
 
+import scala.util.Try
+
 case object HasMadeSalesPage extends QuestionPage[Boolean] {
 
   override def path: JsPath = JsPath \ toString
@@ -28,14 +30,22 @@ case object HasMadeSalesPage extends QuestionPage[Boolean] {
   override def toString: String = "hasMadeSales"
 
   override protected def navigateInNormalMode(answers: UserAnswers): Call = answers.get(HasMadeSalesPage) match {
-    case Some(true)      => routes.DateOfFirstSaleController.onPageLoad(NormalMode)
-    case Some(false)     => routes.IsPlanningFirstEligibleSaleController.onPageLoad(NormalMode)
-    case _               => routes.JourneyRecoveryController.onPageLoad()
+    case Some(true)  => routes.DateOfFirstSaleController.onPageLoad(NormalMode)
+    case Some(false) => routes.IsPlanningFirstEligibleSaleController.onPageLoad(NormalMode)
+    case _           => routes.JourneyRecoveryController.onPageLoad()
   }
 
   override protected def navigateInCheckMode(answers: UserAnswers): Call = answers.get(HasMadeSalesPage) match {
-    case Some(true)      => routes.DateOfFirstSaleController.onPageLoad(CheckMode)
-    case Some(false)     => routes.IsPlanningFirstEligibleSaleController.onPageLoad(CheckMode)
-    case _               => routes.JourneyRecoveryController.onPageLoad()
+    case Some(true)  => routes.DateOfFirstSaleController.onPageLoad(CheckMode)
+    case Some(false) => routes.IsPlanningFirstEligibleSaleController.onPageLoad(CheckMode)
+    case _           => routes.JourneyRecoveryController.onPageLoad()
+  }
+
+  override def cleanup(value: Option[Boolean], userAnswers: UserAnswers): Try[UserAnswers] = {
+    value match {
+      case Some(true)  => userAnswers.remove(IsPlanningFirstEligibleSalePage)
+      case Some(false) => userAnswers.remove(DateOfFirstSalePage)
+      case _           => super.cleanup(value, userAnswers)
+    }
   }
 }
