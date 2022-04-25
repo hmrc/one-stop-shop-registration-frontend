59b2a4d140333b666a2ec37c95f9f8db4af5abd9
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2022-04-25 15:35:26 +0100

    VEOSS-980 | Initial commit - Add new page for EuSendGoodsTradingName

--- test/generators/UserAnswersEntryGenerators.scala
+++ test/generators/UserAnswersEntryGenerators.scala
@@ -26,6 +26,14 @@ import play.api.libs.json.{JsValue, Json}
 
 trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
 
+  implicit lazy val arbitraryEuSendGoodsTradingNameUserAnswersEntry: Arbitrary[(EuSendGoodsTradingNamePage.type, JsValue)] =
+    Arbitrary {
+      for {
+        page  <- arbitrary[EuSendGoodsTradingNamePage.type]
+        value <- arbitrary[String].suchThat(_.nonEmpty).map(Json.toJson(_))
+      } yield (page, value)
+    }
+
   implicit lazy val arbitraryIsPlanningFirstEligibleSaleUserAnswersEntry: Arbitrary[(IsPlanningFirstEligibleSalePage.type, JsValue)] =
     Arbitrary {
       for {
