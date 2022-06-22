154b1156a81844da00c35efbc16ece48640dfba9
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2022-06-22 10:41:02 +0100

    VEOSS-1093| added eu details summary list row to the expected view builder in spec base

--- test/base/SpecBase.scala
+++ test/base/SpecBase.scala
@@ -38,7 +38,7 @@ import play.api.test.FakeRequest
 import services.DateService
 import uk.gov.hmrc.auth.core.retrieve.Credentials
 import uk.gov.hmrc.domain.Vrn
-import viewmodels.checkAnswers.euDetails.TaxRegisteredInEuSummary
+import viewmodels.checkAnswers.euDetails.{EuDetailsSummary, TaxRegisteredInEuSummary}
 import viewmodels.checkAnswers.previousRegistrations.PreviouslyRegisteredSummary
 import viewmodels.checkAnswers._
 
@@ -141,6 +141,7 @@ trait SpecBase
       IsPlanningFirstEligibleSaleSummary.row(answers),
       new CommencementDateSummary(dateService).row(answers),
       TaxRegisteredInEuSummary.row(answers),
+      EuDetailsSummary.checkAnswersRow(answers),
       PreviouslyRegisteredSummary.row(answers),
       IsOnlineMarketplaceSummary.row(answers),
       HasWebsiteSummary.row(answers),
