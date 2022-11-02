35b621a6715dac8ab115ba7263d6b8dd1883cb4d
DevAndyroo <andy@andyroo.info>
2022-11-02 18:04:32 +0000

    VEOSS-1297 add previous ioss number page

--- test/generators/UserAnswersGenerator.scala
+++ test/generators/UserAnswersGenerator.scala
@@ -22,18 +22,17 @@ import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.TryValues
 import pages._
 import pages.euDetails._
-import pages.previousRegistrations.{AddPreviousRegistrationPage, PreviousEuCountryPage, PreviousEuVatNumberPage, PreviousIossSchemePage, PreviouslyRegisteredPage, PreviousSchemePage}
+import pages.previousRegistrations.{AddPreviousRegistrationPage, PreviousEuCountryPage, PreviousEuVatNumberPage, PreviousIossNumberPage, PreviousIossSchemePage, PreviouslyRegisteredPage}
 import play.api.libs.json.{Json, JsValue}
 
 trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
+    arbitrary[(PreviousIossNumberPage.type, JsValue)] ::
     arbitrary[(PreviousIossSchemePage.type, JsValue)] ::
-//    arbitrary[(PreviousSchemePage, JsValue)] ::
     arbitrary[(EuSendGoodsPage, JsValue)] ::
     arbitrary[(EuSendGoodsTradingNamePage, JsValue)] ::
-//    arbitrary[(EuSendGoodsAddressPage.typ, JsValue)] ::
     arbitrary[(IsPlanningFirstEligibleSalePage.type, JsValue)] ::
     arbitrary[(SalesChannelsPage.type, JsValue)] ::
     arbitrary[(HasFixedEstablishmentInNiPage.type, JsValue)] ::
