4b33dc130b3a612a94bc2e4058a8df0216eaf927
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2023-01-17 17:23:56 +0000

    VEOSS-1407 | Fix registration number scheme change bug

--- test/forms/mappings/PreviousRegistrationSchemeConstraintSpec.scala
+++ test/forms/mappings/PreviousRegistrationSchemeConstraintSpec.scala
@@ -16,7 +16,7 @@
 
 package forms.mappings
 
-import models.{Country, PreviousScheme, PreviousSchemeType}
+import models.{Country, Index, PreviousScheme, PreviousSchemeType}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import play.api.data.validation.{Invalid, Valid}
@@ -27,35 +27,98 @@ class PreviousRegistrationSchemeConstraintSpec extends AnyFreeSpec with Matchers
 
   "validatePreviousRegistrationScheme" - {
 
-    "must return Valid when one OSS scheme is passed" in {
+    "existing OSS schemes" - {
 
-      val previousOssScheme: Seq[PreviousScheme] = Seq(
-        PreviousScheme.OSSU
-      )
-      val result = validatePreviousRegistrationSchemes(country.name,previousOssScheme,
-        "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error")(PreviousSchemeType.OSS)
-      result mustEqual Valid
+      "must return Valid when there's no existing OSS scheme" in {
+
+        val existingSchemes: Seq[PreviousScheme] = Seq(
+          PreviousScheme.OSSU
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, existingSchemes,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(0))(PreviousSchemeType.OSS)
+        result mustEqual Valid
+      }
+
+      "must return Valid when there's one existing OSS scheme" in {
+
+        val existingSchemes: Seq[PreviousScheme] = Seq(
+          PreviousScheme.OSSU
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, existingSchemes,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(1))(PreviousSchemeType.OSS)
+        result mustEqual Valid
+      }
+
+      "must return Valid when there are two existing OSS schemes" in {
+
+        val existingSchemes: Seq[PreviousScheme] = Seq(
+          PreviousScheme.OSSU, PreviousScheme.OSSNU
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, existingSchemes,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(2))(PreviousSchemeType.OSS)
+        result mustEqual Invalid("previousScheme.oss.exceed.error", country.name)
+      }
     }
 
-    "must return Invalid when two OSS schemes are passed" in {
+    "editing existing OSS schemes" - {
 
-      val previousOssScheme: Seq[PreviousScheme] = Seq(
-        PreviousScheme.OSSU, PreviousScheme.OSSNU
-      )
-      val result = validatePreviousRegistrationSchemes(country.name, previousOssScheme,
-        "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error")(PreviousSchemeType.OSS)
-      result mustEqual Invalid("previousScheme.oss.exceed.error", country.name)
+      "must return Valid when there's one existing OSS scheme" in {
+
+        val existingSchemes: Seq[PreviousScheme] = Seq(
+          PreviousScheme.OSSU
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, existingSchemes,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(0))(PreviousSchemeType.OSS)
+        result mustEqual Valid
+      }
+
+      "must return Valid when there are two existing OSS schemes" in {
+
+        val existingSchemes: Seq[PreviousScheme] = Seq(
+          PreviousScheme.OSSU, PreviousScheme.OSSNU
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, existingSchemes,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(1))(PreviousSchemeType.OSS)
+        result mustEqual Valid
+      }
+
     }
 
-    "must return Invalid when IOSS scheme is passed" in {
+    "exiting IOSS schemes" - {
 
-      val previousIossScheme: Seq[PreviousScheme] = Seq(
-        PreviousScheme.IOSSWI
-      )
-      val result = validatePreviousRegistrationSchemes(country.name, previousIossScheme,
-        "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error")(PreviousSchemeType.IOSS)
-      result mustEqual Invalid("previousScheme.ioss.exceed.error", country.name)
+      "must return Valid when there's one existing IOSS scheme" in {
+
+        val previousIossScheme: Seq[PreviousScheme] = Seq.empty
+        val result = validatePreviousRegistrationSchemes(country.name, previousIossScheme,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(0))(PreviousSchemeType.IOSS)
+        result mustEqual Valid
+      }
+
+      "must return Invalid when there's two existing IOSS schemes" in {
+
+        val previousIossScheme: Seq[PreviousScheme] = Seq(
+          PreviousScheme.IOSSWI
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, previousIossScheme,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(1))(PreviousSchemeType.IOSS)
+        result mustEqual Invalid("previousScheme.ioss.exceed.error", country.name)
+      }
+    }
+
+    "editing existing IOSS schemes" - {
+
+      "must return Valid when there's one existing IOSS scheme" in {
+
+        val previousIossScheme: Seq[PreviousScheme] = Seq(
+          PreviousScheme.IOSSWI
+        )
+        val result = validatePreviousRegistrationSchemes(country.name, previousIossScheme,
+          "previousScheme.oss.exceed.error", "previousScheme.ioss.exceed.error", Index(0))(PreviousSchemeType.IOSS)
+        result mustEqual Valid
+      }
+
     }
   }
 
 }
+
