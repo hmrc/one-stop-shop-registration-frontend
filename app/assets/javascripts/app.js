b9d280edf1752fbdedd769e9cb8aa0b65d107f1f
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2023-02-23 16:31:14 +0000

    VEOSS-1393 | Add processing wheel to CYA submit

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -103,3 +103,16 @@ if (typeof accessibleAutocomplete != 'undefined' && document.querySelector('.aut
     }
   }
 }
+
+function showTheSpinner() {
+
+  //make the spinning wheel visible
+  document.getElementById("processing-wheel").hidden = false;
+
+  //make the button hidden
+  document.getElementById("continue").style.visibility = 'hidden';
+  document.getElementById("saveProgress").style.visibility = 'hidden';
+
+
+  return false;
+}
