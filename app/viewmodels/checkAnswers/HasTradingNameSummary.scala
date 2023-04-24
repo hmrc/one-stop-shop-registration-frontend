fbc9d35140e03f9b38e80cb90f9f0b230a0ac617
DevAndyroo <andy@andyroo.info>
2023-04-24 12:01:47 +0100

    VEOSS-1561 add AmendMode, basic change your registration controller and connector to backend

--- app/viewmodels/checkAnswers/HasTradingNameSummary.scala
+++ app/viewmodels/checkAnswers/HasTradingNameSummary.scala
@@ -17,7 +17,7 @@
 package viewmodels.checkAnswers
 
 import controllers.routes
-import models.{Mode, UserAnswers}
+import models._
 import pages.HasTradingNamePage
 import play.api.i18n.Messages
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
