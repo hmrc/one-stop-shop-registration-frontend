0bd3bde0046afe6c493b4d0171fafe87d00e3d43
DevAndyroo <andy@andyroo.info>
2023-05-21 22:47:46 +0100

    VEOSS-1596 add amend failure page and remove save for later on failure of amendmnet

--- app/controllers/amend/ChangeYourRegistrationController.scala
+++ app/controllers/amend/ChangeYourRegistrationController.scala
@@ -56,7 +56,6 @@ class ChangeYourRegistrationController @Inject()(
                                                   view: ChangeYourRegistrationView,
                                                   emailService: EmailService,
                                                   dateService: DateService,
-                                                  saveForLaterService: SaveForLaterService,
                                                   frontendAppConfig: FrontendAppConfig
                                                 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with Logging with CompletionChecks {
 
@@ -119,10 +118,7 @@ class ChangeYourRegistrationController @Inject()(
             case Left(e) =>
               logger.error(s"Unexpected result on submit: ${e.toString}")
               auditService.audit(RegistrationAuditModel.build(RegistrationAuditType.AmendRegistration, registration, SubmissionResult.Failure, request))
-              saveForLaterService.saveAnswers(
-                routes.ErrorSubmittingRegistrationController.onPageLoad(),
-                amendRoutes.ChangeYourRegistrationController.onPageLoad()
-              )
+              Redirect(amendRoutes.ErrorSubmittingAmendmentController.onPageLoad()).toFuture
           }
 
         case Invalid(errors) =>
@@ -136,10 +132,7 @@ class ChangeYourRegistrationController @Inject()(
             val errorList = errors.toChain.toList
             val errorMessages = errorList.map(_.errorMessage).mkString("\n")
             logger.error(s"Unable to create a registration request from user answers: $errorMessages")
-            saveForLaterService.saveAnswers(
-              routes.ErrorSubmittingRegistrationController.onPageLoad(),
-              amendRoutes.ChangeYourRegistrationController.onPageLoad()
-            )
+            Redirect(amendRoutes.ErrorSubmittingAmendmentController.onPageLoad()).toFuture
           }
       }
   }
