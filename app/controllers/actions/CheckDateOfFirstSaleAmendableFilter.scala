316866628b43495b007ce90200cafdb46f538551
DevAndyroo <andy@andyroo.info>
2023-05-26 15:52:57 +0100

    VEOSS-1568 use different logic for date of first sale

---
+++ app/controllers/actions/CheckDateOfFirstSaleAmendableFilter.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import logging.Logging
+import models.{AmendMode, Mode}
+import models.requests.AuthenticatedDataRequest
+import play.api.mvc.{ActionFilter, Result}
+import play.api.mvc.Results.Redirect
+import services.RegistrationService
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class CheckDateOfFirstSaleAmendableFilterImpl(mode: Option[Mode],
+                                              registrationService: RegistrationService
+                                           )(implicit val executionContext: ExecutionContext)
+  extends ActionFilter[AuthenticatedDataRequest] with Logging {
+
+  override protected def filter[A](request: AuthenticatedDataRequest[A]): Future[Option[Result]] = {
+
+    implicit val headerCarrier: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
+    implicit val req: AuthenticatedDataRequest[A] = request
+
+    mode match {
+      case Some(AmendMode) =>
+        registrationService.isDateOfFirstSaleAmendable(request.registration).map {
+          case false => Some(Redirect(controllers.amend.routes.NoLongerAmendableController.onPageLoad().url))
+          case _ => None
+        }
+      case _ => Future.successful(None)
+    }
+
+  }
+}
+
+class CheckDateOfFirstSaleAmendableFilterProvider @Inject()(
+                                                           registrationService: RegistrationService
+                                                         )(implicit val executionContext: ExecutionContext) {
+  def apply(mode: Option[Mode]): CheckDateOfFirstSaleAmendableFilterImpl = {
+    new CheckDateOfFirstSaleAmendableFilterImpl(mode, registrationService)
+  }
+}
+
