e9dc0c221bf2315c4c4c0e7f891c7de0ea16ff59
kapoor-prateek <kapoor-prateek@users.noreply.github.com>
2023-06-14 15:56:13 +0100

    VEOSS-1629: User without OSS enrolment can access amend registration journey

--- app/controllers/actions/CheckRegistrationFilter.scala
+++ app/controllers/actions/CheckRegistrationFilter.scala
@@ -22,12 +22,15 @@ import controllers.routes
 import logging.Logging
 import models.requests.AuthenticatedIdentifierRequest
 import models.{AmendLoopMode, AmendMode, Mode}
+import play.api.http.Status.NO_CONTENT
 import play.api.mvc.Results.Redirect
 import play.api.mvc.{ActionFilter, Result}
 import services.DataMigrationService
 import uk.gov.hmrc.auth.core.Enrolments
+import uk.gov.hmrc.domain.Vrn
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
+import utils.FutureSyntax.FutureOps
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -43,37 +46,56 @@ class CheckRegistrationFilterImpl(mode: Option[Mode],
 
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
 
-    for {
+    (for {
       mayBeRegistration <- connector.getRegistration()
     } yield {
 
       if (mode.contains(AmendMode) || mode.contains(AmendLoopMode)) {
         mayBeRegistration match {
-          case Some(_) => None
-          case _ => Some(Redirect(routes.NotRegisteredController.onPageLoad()))
+
+          case Some(_) if hasRegistrationEnrolment(request.enrolments) => None.toFuture
+
+          case Some(_) => enrolRegisteredUser(request.vrn)
+
+          case _ => Some(Redirect(routes.NotRegisteredController.onPageLoad())).toFuture
         }
       } else if (mayBeRegistration.isDefined || hasRegistrationEnrolment(request.enrolments)) {
         request.queryString.get("k").flatMap(_.headOption).map(sessionId =>
           migrationService
             .migrate(sessionId, request.userId)
         )
-        Some(Redirect(routes.AlreadyRegisteredController.onPageLoad()))
+        Some(Redirect(routes.AlreadyRegisteredController.onPageLoad())).toFuture
       } else {
-        None
+        None.toFuture
       }
-    }
+    }).flatten
   }
 
   private def hasRegistrationEnrolment(enrolments: Enrolments): Boolean = {
     config.enrolmentsEnabled && enrolments.enrolments.exists(_.key == config.ossEnrolment)
   }
+
+  private def enrolRegisteredUser(vrn: Vrn)
+                                           (implicit hc: HeaderCarrier): Future[Option[Result]] = {
+
+    connector.enrolUser().flatMap { response =>
+      response.status match {
+        case NO_CONTENT =>
+          logger.info(s"Successfully retrospectively enrolled user ${vrn.vrn}")
+          None.toFuture
+        case status =>
+          logger.error(s"Failure enrolling an existing user, got status $status from registration service")
+          throw new IllegalStateException("Existing user didn't have enrolment and was unable to enrol user")
+      }
+    }
+  }
 }
 
 class CheckRegistrationFilterProvider @Inject()(
-                                             connector: RegistrationConnector,
-                                             config: FrontendAppConfig,
-                                             migrationService: DataMigrationService
-                                           )(implicit ec: ExecutionContext) {
+                                                 connector: RegistrationConnector,
+                                                 config: FrontendAppConfig,
+                                                 migrationService: DataMigrationService
+                                               )(implicit ec: ExecutionContext) {
   def apply(mode: Option[Mode]): CheckRegistrationFilterImpl = {
     new CheckRegistrationFilterImpl(mode, connector, config, migrationService)
   }
