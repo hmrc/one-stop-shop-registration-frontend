4ff9c5263456537bb720ee1cdb1660935788c2bc
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-10-10 15:11:13 +0100

    VEOSS-1965 | Update missed library versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.5.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.13.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % "9.5.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.2.0",
     "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0",
