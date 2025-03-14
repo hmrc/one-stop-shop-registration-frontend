e8fc4a3a8c355e0fb3c04c098971e5e1818b0ab9
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2025-03-14 15:48:47 +0000

    VEOSS-2072: Fix Tests implement Get Current Returns to RejoinRegistrationController

--- app/connectors/ReturnStatusConnector.scala
+++ app/connectors/ReturnStatusConnector.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors
 
 import config.Service
@@ -13,7 +29,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class ReturnStatusConnector @Inject()(config: Configuration, httpClientV2: HttpClientV2)
                                      (implicit ec: ExecutionContext) extends HttpErrorFunctions {
 
-  private val baseUrl = config.get[Service]("microservice.services.one-stop-shop-registration")
+  private val baseUrl = config.get[Service]("microservice.services.one-stop-shop-returns")
 
   def getCurrentReturns(vrn: Vrn)(implicit hc: HeaderCarrier): Future[CurrentReturnsResponse] =
     httpClientV2.get(url"$baseUrl/vat-returns/current-returns/$vrn").execute[CurrentReturnsResponse]
