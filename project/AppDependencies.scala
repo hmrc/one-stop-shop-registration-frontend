0175411556efa8da80bf84ea4697efd2f572336b
Dorothea Toeller <147050615+Dorothea87@users.noreply.github.com>
2026-02-19 14:13:41 +0000

    VEI-789 Fix flaky tests (#465)
    * VEI-789 remove scalatest override from app dependencies
    
    * VEI-789 update dependencies
    
    * VEI-789 set date to align with form validation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ object AppDependencies {
   import play.core.PlayVersion
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.11.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.27.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.31.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
@@ -17,7 +17,6 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"               % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.2",
