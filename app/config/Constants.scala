1d33e2b045d4da2afb42d8dc5da5fc7401061d9b
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2026-04-15 10:53:14 +0100

    VEOSS-1958 | Remove redundant constants.

--- app/config/Constants.scala
+++ app/config/Constants.scala
@@ -28,9 +28,6 @@ object Constants {
   val maxOssSchemes: Int = 2
   val maxIossSchemes: Int = 1
 
-  val registrationConfirmationTemplateId = "oss_registration_confirmation"
-  val amendRegistrationConfirmationTemplateId = "oss_registration_amendment_confirmation"
-
   val schemeStartDate: LocalDate = LocalDate.of(2021, 7, 1)
 
   val emailVerificationMaxEmails: Int = 10
